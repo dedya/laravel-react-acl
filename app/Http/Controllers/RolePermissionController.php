b38eaf2a325e7c1bcd4072a959aba30384b07b1b
Dedy Adhiewirawan <dedy.adhiewirawan@gmail.com>
2025-06-13 14:56:41 +0700

    apply permission for rolepermission page

--- app/Http/Controllers/RolePermissionController.php
+++ app/Http/Controllers/RolePermissionController.php
@@ -11,10 +11,10 @@ class RolePermissionController extends Controller
 {
 
     function __construct() {
-        /*$this->middleware('permission:create-role')->only('create', 'store', 'enable','disable');
+        $this->middleware('permission:create-role')->only('create', 'store', 'enable','disable');
         $this->middleware('permission:read-role')->only('index','edit');
         $this->middleware('permission:update-role')->only('update','enable','disable');
-        $this->middleware('permission:delete-role')->only('destroy');*/
+        $this->middleware('permission:delete-role')->only('destroy');
     }
 
     public function index()

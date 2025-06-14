2906f4e04eb2a9030e6c3592f36abafee85845b7
Dedy Adhiewirawan <dedy.adhiewirawan@gmail.com>
2025-06-14 20:27:43 +0700

    revert to display user group

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,6 @@ use Laravel\Sanctum\HasApiTokens;
 
 use Spatie\Permission\Traits\HasRoles;
 use Spatie\Permission\Traits\HasPermissions;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * @property \Illuminate\Support\Collection $permissions
@@ -20,7 +19,6 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
     use HasRoles, HasPermissions;
-    use SoftDeletes;
     /**
      * The attributes that are mass assignable.
      *
@@ -30,7 +28,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        //'user_group_id',
+        'user_group_id',
         'is_active',
         'photo'
     ];
